31542a8c95da56c643c199382f3eb68dca1da351
Abhinav Kumar <work.abhinavkr@gmail.com>
2021-06-05 23:34:50 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../Components/Footer'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import AboutSection from '../components/home/AboutSection'
 import IntroSection from '../components/home/IntroSection'
