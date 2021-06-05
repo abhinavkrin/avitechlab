28ff7f4eedd1c00b93202051cb6554f79c8bd636
Abhinav Kumar <15830206+abhinavkrin@users.noreply.github.comm>
2021-06-06 02:05:13 +0530

    header effects

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,44 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import Link from 'next/link';
 import styles from '../styles/components/Header.module.scss';
 function Header({isHome}){
+    const SCROLL_FLAG_POINT = 150;
     const navRef = useRef();
+    const [isTransparent,setIsTransparent] = useState(false);
     const toggle = () => {
         if(navRef.current){
             navRef.current.classList.toggle(styles.show);
         }
     }
+    useEffect(()=>{
+        if(!isHome)
+            return;
+        const checkHeaderType = () => {
+            if(!isHome)
+                return;
+            let flag;
+            if(window.scrollY > SCROLL_FLAG_POINT && isTransparent){
+                flag = false;
+            }
+            if(window.scrollY <= SCROLL_FLAG_POINT && !isTransparent){
+                flag = true;
+            }
+            if(flag !== isTransparent){
+                setIsTransparent(flag);
+            }
+        }
+        checkHeaderType();
+        window.addEventListener("scroll",checkHeaderType); 
+        return (
+            () => {
+                window.removeEventListener("scroll",checkHeaderType);
+            }
+        )
+    },[])
     return (
-        <header className={"fixed-top "+styles.header+" "+styles.transparent}>
+        <header className={"fixed-top "+styles.header+" "+(isTransparent?styles.transparent:"")}>
             <nav ref={navRef}>
                 <div className={styles.brand}>
                     <Image src="/images/icon.svg" width="48" height="48"></Image>
